6cbfde22391be6ec841ec45178d192e04779b17c
bharatkalyan <s.bharatkalyan@gmail.com>
2021-03-11 09:28:21 +0530

    added routes

--- routes/router.js
+++ routes/router.js
@@ -1,21 +1,8 @@
-var express = require('express');
-    router = express.Router();
-    const controller=require('../controller/index');
-    const config = require('../config/config')
-
-router.get('/', ()=>{
-  controller.coronaResult
-} );
-
-router.get('/', function(req, res, next) {
-  request({
-    uri: config.url,
-  }).pipe(res);
-});
-
-
-// console.log("host",q.host); //returns 'localhost:8080'
-// console.log("pathname",q.pathname); //returns '/default.htm'
-// console.log("search",q.search); //returns '?year=2017&month=february'
-
-module.exports=router;
+module.exports = app => {
+  const controller = require("../controller/index");
+  var router = require("express").Router();
+  router.get("/", (req,res)=>{
+    res.setHeader('Content-Type', 'text/html');
+    controller.callExternalApiUsingHttp
+  });
+};
